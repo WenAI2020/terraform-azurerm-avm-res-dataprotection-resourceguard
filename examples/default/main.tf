018995052f4483ee602aacd6308c574d9d725002
Wen <twbigs@gmail.com>
2025-04-21 21:39:30 +0800

    resource guard is tested in examples/default

--- examples/default/main.tf
+++ examples/default/main.tf
@@ -1,63 +1,45 @@
 terraform {
-  required_version = "~> 1.5"
+  required_version = ">= 1.9, < 2.0"
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 4.21"
-    }
-    modtm = {
-      source  = "azure/modtm"
-      version = "~> 0.3"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.5"
+      version = "~> 4.0"
     }
   }
 }
 
 provider "azurerm" {
-  features {}
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }    
+  }
+  subscription_id = "eeed4cd0-013c-43a7-8e45-dd765abaff2c"
+ # resource_provider_registrations = "all"
 }
 
-
-## Section to provide a random Azure region for the resource group
-# This allows us to randomize the region for the resource group.
-module "regions" {
-  source  = "Azure/avm-utl-regions/azurerm"
-  version = "~> 0.1"
+locals {
+  enable_telemetry = true
+  location         = "southeastasia"
+  vault_critical_operation_exclusion_list = [
+    "Microsoft.RecoveryServices/vaults/backupFabrics/protectionContainers/protectedItems/delete"
+  ]
+  tags = {
+    scenario  = "Default"
+    project   = "AVM"
+    delete    = "yes"
+  }
+  resource_group_id = "/subscriptions/eeed4cd0-013c-43a7-8e45-dd765abaff2c/resourceGroups/firstavmrg"
+  resource_group_name = "firstavmrg"
+  name = "commitregu"
 }
 
-# This allows us to randomize the region for the resource group.
-resource "random_integer" "region_index" {
-  max = length(module.regions.regions) - 1
-  min = 0
-}
-## End of section to provide a random Azure region for the resource group
-
-# This ensures we have unique CAF compliant names for our resources.
-module "naming" {
-  source  = "Azure/naming/azurerm"
-  version = "~> 0.3"
-}
-
-# This is required for resource modules
-resource "azurerm_resource_group" "this" {
-  location = module.regions.regions[random_integer.region_index.result].name
-  name     = module.naming.resource_group.name_unique
-}
-
-# This is the module call
-# Do not specify location here due to the randomization above.
-# Leaving location as `null` will cause the module to use the resource group location
-# with a data source.
-module "test" {
-  source = "../../"
-  # source             = "Azure/avm-<res/ptn>-<name>/azurerm"
-  # ...
-  location            = azurerm_resource_group.this.location
-  name                = "TODO" # TODO update with module.naming.<RESOURCE_TYPE>.name_unique
-  resource_group_name = azurerm_resource_group.this.name
-
-  enable_telemetry = var.enable_telemetry # see variables.tf
+module "default"{
+    source = "../../"
+    name = local.name
+    location = local.location
+    vault_critical_operation_exclusion_list = local.vault_critical_operation_exclusion_list
+    resource_group_name = local.resource_group_name
+    resource_group_id = local.resource_group_id
+    tags = local.tags
 }
--- terraform.tf
+++ terraform.tf
@@ -2,14 +2,18 @@ terraform {
   required_version = ">= 1.9, < 2.0"
   required_providers {
     # TODO: Ensure all required providers are listed here and the version property includes a constraint on the maximum major version.
+    azapi = {
+      source  = "azure/azapi"
+      version = ">= 1.13, < 3"
+    }
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 4.0"
+      version = ">= 3.116, < 5"
     }
     modtm = {
       source  = "azure/modtm"
       version = "~> 0.3"
-    }
+    }    
     random = {
       source  = "hashicorp/random"
       version = "~> 3.5"
